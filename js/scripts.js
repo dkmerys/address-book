3307c06f36cc74d9be6efeabf0ae71e48e35b108
Daniel Merys <dkmerys@gmail.com>
2020-04-06 15:11:59 -0700

    add delete contact functionality

--- js/scripts.js
+++ js/scripts.js
@@ -60,9 +60,25 @@ function displayContactDetails(addressBookToDisplay){
   contactsList.html(htmlForContactInfo);
 };
 
+function showContact(contactId) {
+  var contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  var buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
+}
+
 function attachContactListeners() {
   $("ul#contacts").on("click", "li", function() {
-    console.log("The id of this <li> is " + this.id + ".");
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function(){
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
   });
 };
 
